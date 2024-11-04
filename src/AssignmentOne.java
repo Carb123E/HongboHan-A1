0b38cd0a49db19c2e4b42b3441527cca83822912
lxx <2020814343@qq.com>
2024-11-04 17:35:19 +0800

    Perform final testing in the main method

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -3,8 +3,9 @@ import java.util.Iterator;
 import java.util.List;
 
 public class AssignmentOne {
-    List<Appointment> appointments = new ArrayList<>();
+    public static List<Appointment> appointments = new ArrayList<>();
     public static void main(String[] args) {
+        //Part 3
         GeneralPractitioner gp1 = new GeneralPractitioner(1, "Dr. Smith", "Experienced in general practice", "Internal Medicine");
         GeneralPractitioner gp2 = new GeneralPractitioner(2, "Dr. Johnson", "Specializes in pediatrics", "Pediatrics");
         GeneralPractitioner gp3 = new GeneralPractitioner(3, "Dr. Williams", "Expert in family medicine", "Family Medicine");
@@ -22,6 +23,15 @@ public class AssignmentOne {
         System.out.println("-------------------------------------");
         ohp2.printDetails();
         System.out.println("-------------------------------------");
+
+        //Part 5
+        createAppointment(appointments, "John Doe", "1234567890", "08:00", gp1);
+        createAppointment(appointments, "Jane Smith", "0987654321", "09:00", gp2);
+        createAppointment(appointments, "Alice Brown", "1122334455", "10:00", ohp1);
+        createAppointment(appointments, "Bob Davis", "6677889900", "11:00", ohp2);
+        printExistingAppoints(appointments);
+        cancelBooking(appointments, "1234567890");
+        printExistingAppoints(appointments);
     }
 
     public static void createAppointment(List<Appointment> appointments, String patientName, String phoneNumber, String preferredTime, HealthProfessional healthProfessional) {
